e9a00dcc1ed50f618aa5140a40f95f3f01a087c8
IreriVikki <wambsviki@gmail.com>
2018-10-17 15:58:28 +0300

    added views for login, logout registration and home

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render, redirect, get_object_or_404, reverse, get_list_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.core.mail import mail_admins
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+from .forms import *
+from .models import *
+import datetime
 
 # Create your views here.
+
+
+def home(request):
+    context = {
+        'user': request.user
+    }
+    return render(request, 'index.html', context)
+
+
+def login(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            # correct username and password login the user
+            auth.login(request, user)
+            return redirect('home')
+
+        else:
+            messages.error(request, 'Error wrong username/password')
+
+    return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = MyRegistrationForm(request.POST)
+        if form.is_valid():
+            print('here')
+            form.save()
+            return redirect('login')
+
+    form = MyRegistrationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'register.html', context)
