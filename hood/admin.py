1bf93a7f6a25deaaad4f6c07c76425c23065e808
IreriVikki <wambsviki@gmail.com>
2018-10-18 09:57:07 +0300

    added my models to the admin page

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,22 @@
+from __future__ import unicode_literals
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class NeighbourHoodAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user_name',)
+
+
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(NeighbourHood, NeighbourHoodAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Business, BusinessAdmin)
